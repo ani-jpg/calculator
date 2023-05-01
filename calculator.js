fbc5e2e1ee4717638f23d229a8ab846cf74f4558
Annie <113863477+anniet22@users.noreply.github.com>
2023-05-01 20:15:56 -0300

    fixed the func so it accounts for the first time u use the calc, doesn't really work tho bc it carries out like 1+1 only if u click 1+1+ so i need to fiugre that out

--- calculator.js
+++ calculator.js
@@ -1,79 +1,74 @@
 function add (a,b) {
     return a+b; 
-    
     };
-
-console.log(add(1,2));
 
 function subtract(a, b) {
 	return a - b;
 };
 
-console.log(subtract(3,1));
-
 function multiply(a,b) {
     return a * b;
   };
-
-  console.log(multiply(3,4));
 
 function divide(a,b) {
     return a / b;
 }
 
-console.log(divide(12,6));
-
-var operator;
-
-function operate(a, operator, b) {
-    if (operator=='+') {
-        add (a, b);
-    }
-}
-
-console.log(operate(1,'+',3));
 
 const buttons= document.querySelectorAll('button');
 const display= document.querySelector('#display');
 
 function calcDisplay() {
 buttons.forEach((button)=> { button.addEventListener ('click', () => {
-    console.log(button.id);
     display.textContent= button.id;
 })
 })
 };
-
 calcDisplay();
 
-const numberBtns= document.querySelectorAll('.number-btn')
-const operatorBtns=document.querySelectorAll('.operator-btn')
-console.log(operatorBtns);
-
-function add (a,b) {
-    return a+b; 
-    
-    };
-
-let total= 0;
+let total;
+let operator;
 function math() {
 let result= '';
     buttons.forEach((button)=> {button.addEventListener ('click', () => {
             if (button.classList=='number-btn') {
                 result += button.id;
                 display.textContent=result;
+                console.log(total, 'heylo');
                 return result;
                 //need to store the variable- done, its stored as result 
             }
             
             if (button.classList=='operator-btn') {
                 //store number as operator & if next is also a numb then you execute func of operator
-                console.log(+result, button.id);
-                total= add (+result, total);
+                
+                if (isNaN(total)) {
+                    total= +result;
+                    console.log(total, 'nan')
+                }
+
+                else {
+                    
+                if (button.id=='add') {
+                    total= add (+result, total);
+                }
+    
+                    else if (button.id=='subtract') {
+                        total= subtract(total, +result)
+                    }
+    
+                    else if (button.id=='multiply') {
+                        total= multiply(+result, total);
+                    }
+                    
+                }
+                
                 result = ' ';
             }
             console.log(total, 'ayo');
             return total;
+            
+            
     
     })  
     })
