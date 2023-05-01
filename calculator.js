72ef5b17961bcd30e4429305559ea7066f9f474e
Annie <113863477+anniet22@users.noreply.github.com>
2023-05-01 18:53:46 -0300

    Created grid for calc, calcdisplay function and started math func

--- calculator.js
+++ calculator.js
@@ -23,8 +23,6 @@ function divide(a,b) {
 
 console.log(divide(12,6));
 
-var firstNum;
-var secondNum;
 var operator;
 
 function operate(a, operator, b) {
@@ -35,3 +33,51 @@ function operate(a, operator, b) {
 
 console.log(operate(1,'+',3));
 
+const buttons= document.querySelectorAll('button');
+const display= document.querySelector('#display');
+
+function calcDisplay() {
+buttons.forEach((button)=> { button.addEventListener ('click', () => {
+    console.log(button.id);
+    display.textContent= button.id;
+})
+})
+};
+
+calcDisplay();
+
+const numberBtns= document.querySelectorAll('.number-btn')
+const operatorBtns=document.querySelectorAll('.operator-btn')
+console.log(operatorBtns);
+
+function add (a,b) {
+    return a+b; 
+    
+    };
+function math() {
+let result= '';
+    buttons.forEach((button)=> {button.addEventListener ('click', () => {
+        let total= 0;
+    
+            if (button.classList=='number-btn') {
+                result += button.id;
+                display.textContent=result;
+                return result;
+                //need to store the variable- done, its stored as result 
+            }
+            
+            if (button.classList=='operator-btn') {
+                //store number as operator & if next is also a numb then you execute func of operator
+                console.log(+result, button.id);
+                total= add (+result, total);
+                result = ' ';
+            }
+            console.log(total, 'ayo');
+            return total;
+    
+    })  
+    })
+    
+};
+
+math ();
